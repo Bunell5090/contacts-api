330fa3b115aa6656abf2d0403286763c0be6798f
Bunell5090 <Bunell5090.com>
2022-03-21 20:47:27 -0500

    Added sessions controller & updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
   get "/one_contact", controller: "contacts", action: "get_one_contact"
   get "/all_contacts", controller: "contacts", action: "get_all_contacts"
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
